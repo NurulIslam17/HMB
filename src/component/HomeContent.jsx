cc15919672abf92f671d9a027cfccdfd6f044ab8
@Nurul97 <nurulislamnextgenitltd.com>
2024-07-18 14:04:44 +0600

    HMB setup

---
+++ src/component/HomeContent.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+function HomeContent() {
+  return (
+    <>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-7 mx-auto">
+            <p className="text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos qui ex consequatur saepe deleniti rerum voluptate
+              consectetur, quidem odit officiis eligendi assumenda quos laborum!
+              Iusto reiciendis aliquam velit harum eligendi?
+            </p>
+            <p className="text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos qui ex consequatur saepe deleniti rerum voluptate
+              consectetur, quidem odit officiis eligendi assumenda quos laborum!
+              Iusto reiciendis aliquam velit harum eligendi?
+            </p>
+            <p className="text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos qui ex consequatur saepe deleniti rerum voluptate
+              consectetur, quidem odit officiis eligendi assumenda quos laborum!
+              Iusto reiciendis aliquam velit harum eligendi?
+            </p>
+
+            <p className="text-justify">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+              incidunt dignissimos, commodi autem necessitatibus eos. Deserunt
+              magni itaque, nobis, enim nemo reiciendis, rerum ratione et
+              corporis corrupti inventore placeat perspiciatis explicabo sint
+              totam eaque aut ex libero laudantium repellat. Voluptatum
+              doloremque ad quam optio, repudiandae corrupti excepturi adipisci
+              totam dicta deleniti, maiores rerum maxime cupiditate placeat,
+              perspiciatis impedit vero rem distinctio assumenda in! Unde earum,
+              fuga fugit consequuntur, nam asperiores repellendus at repellat
+              ipsam dolor obcaecati quidem vitae sapiente, consequatur
+              architecto deserunt? Minus perspiciatis impedit, provident vitae
+              eligendi nam perferendis quis molestias dolores totam natus ipsa
+              officiis sit odio accusantium labore deserunt error cumque beatae
+              ab delectus officia. Consequuntur, animi porro omnis
+              reprehenderit, minus temporibus necessitatibus non hic atque
+              molestiae doloremque soluta similique sunt nulla facere
+              voluptatibus nam error nobis deserunt odit quaerat sapiente fugit.
+              Eum dolores laudantium hic esse doloribus quos repellendus earum
+              alias non voluptatem debitis temporibus deserunt perspiciatis
+              maiores est veniam, enim a beatae obcaecati praesentium omnis?
+              Blanditiis facilis mollitia nihil sit quas ipsam earum, minima
+              dignissimos, voluptatum debitis accusamus amet placeat nam et
+              perspiciatis minus, veniam non? Voluptate temporibus eos nam
+              dolorem asperiores magni quasi laborum quia repellendus, molestias
+              quis, ipsum magnam, excepturi et velit enim.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeContent;
---
+++ src/page/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
+import HomeContent from '../component/HomeContent'
+
+function Home() {
+  return (
+    <>
+      <Navbar/>
+      <HomeContent/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home

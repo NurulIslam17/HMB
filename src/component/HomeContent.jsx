b780f61fe93b507220b7f78d8b6bbfe73d093e16
@Nurul97 <nurulislamnextgenitltd.com>
2024-07-18 15:16:31 +0600

    Rooms

--- src/component/HomeContent.jsx
+++ src/component/HomeContent.jsx
@@ -1,58 +1,114 @@
 import React from "react";
+import img1 from "../asset/image/hotel (1).jpg";
+import img2 from "../asset/image/hotel (2).jpg";
+import img3 from "../asset/image/hotel-4.jpg";
 
 function HomeContent() {
   return (
     <>
       <div className="container my-5">
+          <h5>Recent Products</h5>
         <div className="row">
-          <div className="col-md-7 mx-auto">
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos qui ex consequatur saepe deleniti rerum voluptate
-              consectetur, quidem odit officiis eligendi assumenda quos laborum!
-              Iusto reiciendis aliquam velit harum eligendi?
-            </p>
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos qui ex consequatur saepe deleniti rerum voluptate
-              consectetur, quidem odit officiis eligendi assumenda quos laborum!
-              Iusto reiciendis aliquam velit harum eligendi?
-            </p>
-            <p className="text-justify">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos qui ex consequatur saepe deleniti rerum voluptate
-              consectetur, quidem odit officiis eligendi assumenda quos laborum!
-              Iusto reiciendis aliquam velit harum eligendi?
-            </p>
 
-            <p className="text-justify">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
-              incidunt dignissimos, commodi autem necessitatibus eos. Deserunt
-              magni itaque, nobis, enim nemo reiciendis, rerum ratione et
-              corporis corrupti inventore placeat perspiciatis explicabo sint
-              totam eaque aut ex libero laudantium repellat. Voluptatum
-              doloremque ad quam optio, repudiandae corrupti excepturi adipisci
-              totam dicta deleniti, maiores rerum maxime cupiditate placeat,
-              perspiciatis impedit vero rem distinctio assumenda in! Unde earum,
-              fuga fugit consequuntur, nam asperiores repellendus at repellat
-              ipsam dolor obcaecati quidem vitae sapiente, consequatur
-              architecto deserunt? Minus perspiciatis impedit, provident vitae
-              eligendi nam perferendis quis molestias dolores totam natus ipsa
-              officiis sit odio accusantium labore deserunt error cumque beatae
-              ab delectus officia. Consequuntur, animi porro omnis
-              reprehenderit, minus temporibus necessitatibus non hic atque
-              molestiae doloremque soluta similique sunt nulla facere
-              voluptatibus nam error nobis deserunt odit quaerat sapiente fugit.
-              Eum dolores laudantium hic esse doloribus quos repellendus earum
-              alias non voluptatem debitis temporibus deserunt perspiciatis
-              maiores est veniam, enim a beatae obcaecati praesentium omnis?
-              Blanditiis facilis mollitia nihil sit quas ipsam earum, minima
-              dignissimos, voluptatum debitis accusamus amet placeat nam et
-              perspiciatis minus, veniam non? Voluptate temporibus eos nam
-              dolorem asperiores magni quasi laborum quia repellendus, molestias
-              quis, ipsum magnam, excepturi et velit enim.
-            </p>
+          <div className="col mb-3 gap-2">
+            <div className="card w-18rem">
+              <img src={img1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
           </div>
+
+          <div className="col mb-3">
+            <div className="card w-18rem">
+              <img src={img3} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col mb-3">
+            <div className="card w-18rem">
+              <img src={img1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col mb-3">
+            <div className="card w-18rem">
+              <img src={img2} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col mb-3">
+            <div className="card w-18rem">
+              <img src={img1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col mb-3">
+            <div className="card w-18rem">
+              <img src={img1} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          
+
         </div>
       </div>
     </>

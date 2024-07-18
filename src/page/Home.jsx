45c1b588b6979119ceec10ce0d1ea84aae1abf4c
@Nurul97 <nurulislamnextgenitltd.com>
2024-07-18 14:42:46 +0600

    Home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../component/Navbar'
 import Footer from '../component/Footer'
 import HomeContent from '../component/HomeContent'
+import Carouse from '../component/Carouse'
 
 function Home() {
   return (
     <>
       <Navbar/>
+      <Carouse/>
       <HomeContent/>
       <Footer/>
     </>
